db3f5499c66e599eaae42a6997cf6c7872dff8be
oguz turkay <tyserract@gmail.com>
2022-06-16 01:29:49 +0300

    refactor: add protoPath to protobuf registry service with join

--- src/module/user/user.module.ts
+++ src/module/user/user.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ProtobufRegistryService } from '../misc/service/protobuf-registry/protobuf-registry.service';
 import { ProtobufRegistryModule } from '../misc/service/protobuf-registry/protobuf-registry.module';
+import { join } from 'path';
 
 @Module({
   imports: [ProtobufRegistryModule],
@@ -11,7 +12,7 @@ export class UserModule {
   constructor(private protobufRegistryService: ProtobufRegistryService) {
     protobufRegistryService.addService({
       package: 'user',
-      protoPath: __dirname + '/model/user.proto',
+      protoPath: join(__dirname, '/model/user.proto'),
     });
   }
 }
