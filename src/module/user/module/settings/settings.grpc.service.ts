c865393ae0d2c35c3ef242d33214e5f459ab92d7
oguz turkay <tyserract@gmail.com>
2022-06-22 02:39:22 +0300

    fix: getting user from session

--- src/module/user/module/settings/settings.grpc.service.ts
+++ src/module/user/module/settings/settings.grpc.service.ts
@@ -3,7 +3,6 @@ import { GrpcMethod, Payload } from '@nestjs/microservices';
 import { UpdateSettings } from './input/update-settings';
 import { CurrentSession } from '../../../shared/decorator/param/current-session';
 import { plainToClassFromExist } from 'class-transformer';
-import { User } from '../../model/user';
 import { map, Observable, switchMap } from 'rxjs';
 import { Settings } from './model/settings';
 import { Session } from '../session/model/session';
@@ -13,10 +12,12 @@ export class SettingsService {
   @GrpcMethod()
   update(
     @Payload() payload: UpdateSettings,
-    @CurrentSession(['user.settings']) user: Observable<User>,
+    @CurrentSession(['user.settings']) session: Observable<Session>,
   ): Observable<Settings> {
-    return user.pipe(
-      switchMap((user) => plainToClassFromExist(user.settings, payload).save()),
+    return session.pipe(
+      switchMap((session) =>
+        plainToClassFromExist(session.user.settings, payload).save(),
+      ),
     );
   }
 
