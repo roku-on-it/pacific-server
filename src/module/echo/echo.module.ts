3ae69c8b7b19c2dafcf611f96ae48422f9e82754
oguz turkay <tyserract@gmail.com>
2022-06-22 03:18:44 +0300

    style(echo-module): add descriptive comment about echo-module

--- src/module/echo/echo.module.ts
+++ src/module/echo/echo.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EchoService } from './echo.grpc.service';
 
+// Dummy module for example purposes
+
 @Module({ controllers: [EchoService] })
 export class EchoModule {}
